2a688b30e809148fac58a33529bdd472badf1856
sumangalm-1 <mugalikarsumangal@gmail.com>
2022-07-06 10:14:14 +0000

    generate setup.tar

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-docker login -u $docker_username -p $docker_password
+#!/bin/bash
 docker build -f drone-git:linux-ppc64le -f ./docker/Dockerfile.linux.ppc64le .
 docker tag drone/git:ppc64le sbm13/drone/git:ppc64le
 docker push docker.io/sbm13/drone/git:linux-ppc64le
+tar -cvf setup.tar /tmp/remote/greeting
