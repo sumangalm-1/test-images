8db8941a47740252775f37ebd9a5f22fa8df3244
sumangalm-1 <65329251+sumangalm-1@users.noreply.github.com>
2022-07-06 15:28:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+docker login -u $docker_username -p $docker_password
 docker build -f drone-git:linux-ppc64le -f ./docker/Dockerfile.linux.ppc64le .
 docker tag drone/git:ppc64le sbm13/drone/git:ppc64le
 docker push docker.io/sbm13/drone/git:linux-ppc64le
